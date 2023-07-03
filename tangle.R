53340f65bda6edf0429d37aa4e494fa5501e1324
lcolladotor <lcolladotor@gmail.com>
2023-07-03 00:40:59 -0400

    Edit tangle.R to create the .R files

--- tangle.R
+++ tangle.R
@@ -1,4 +1,4 @@
 styler::style_dir(filetype = "Rmd", transformers = biocthis::bioc_style())
 
-# rmds <- dir(pattern = "\\.Rmd$")
-# sapply(rmds, knitr::knit, tangle = TRUE)
+rmds <- dir(pattern = "\\.Rmd$")
+sapply(rmds, knitr::knit, tangle = TRUE)
